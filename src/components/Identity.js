dec14a4adfea3e55d1291bf32bd7902452609d26
Peter Atef <peter.atef@coformatique.com>
2023-05-04 21:42:55 +0200

    stuff

--- src/components/Identity.js
+++ src/components/Identity.js
@@ -1,9 +1,10 @@
 import '../styles/Identity.css'
+import Logo from '../assests/logo.svg'
 
 function Identity(){
     return (
         <div className='identity'>
-            <img className='logo' src={require('../assests/logo.jpeg')} alt="logo" />
+            <img className='logo' src={Logo} alt="logo" />
             <h2 className='title'>Course Dog</h2>
         </div>
     )
