a7319eb52f235725f0d9410c7a90c0ce667cf82b
AbdelrahmanAli-gif <abdelrahmanali58@gmail.com>
2023-05-06 22:58:45 +0300

    search card styling

--- src/components/SearchResultCard.js
+++ src/components/SearchResultCard.js
@@ -1,12 +1,19 @@
-import React from "react";
 import '../styles/SearchResultCard.css'
+
+import ClassroomLogo from '../assests/classroom.svg';
 
 export function SearchResultCard({course}){
     return (
-        <a className="result">
-            <h2>{course.courseName}</h2>
-            <h3>{course.instructor}</h3>
-            <h4>{course.semester}</h4>
-        </a>
+        <div className="result">
+            <div className="course-search-result-info">
+                <h2>{course.courseName}</h2>
+                <h4 className="course-search-result-instructor">{course.instructor}</h4>
+                <h5 className="course-search-result-year">{course.semester}</h5>
+            </div>
+            <div className="course-search-result-platform-container">
+                <img className="course-search-result-platform-image" src={ClassroomLogo} />
+            </div>
+            
+        </div>
     )
 }
